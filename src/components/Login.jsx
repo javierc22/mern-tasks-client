58c10c02b2ba44526a9e625cf436e7b54a4c2b5a
javierc22 <javier.ec.22@gmail.com>
2020-07-08 22:05:45 -0400

    Creando el Formulario de Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 
 const Login = () => {
+
+  const onChange = () => {
+
+  }
+
   return (
-    <h2>Desde Login</h2>
+    <div className="j-form-user">
+      <div className="j-container-form j-shadow-dark">
+        <h1>Iniciar Sesión</h1>
+
+        <form>
+          <div className="j-field-form">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email"
+              id="email"
+              name="email" 
+              placeholder="Tu Email" 
+              onChange={onChange} 
+            />
+          </div>
+          <div className="j-field-form">
+            <label htmlFor="password">Contraseña</label>
+            <input 
+              type="password"
+              id="password"
+              name="password" 
+              placeholder="Tu Contraseña" 
+              onChange={onChange} 
+            />
+          </div>
+
+          <div className="j-field-form">
+            <input 
+              type="submit"
+              className="j-btn j-btn-primary j-btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
