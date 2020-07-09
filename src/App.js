1de13f087dfd8dda791899ecd22e589fc8d4f91d
javierc22 <javier.ec.22@gmail.com>
2020-07-08 22:23:04 -0400

    FIX estructura de rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from './components/Login';
-import NewAccount from './components/NewAccount';
-import Proyects from './components/Proyects';
+import Login from './components/auth/Login';
+import NewAccount from './components/auth/NewAccount';
+import Proyects from './components/proyects/Proyects';
 
 
 function App() {
