be988a78ef370040f8ead1d28a2105b35af7a750
javierc22 <javier.ec.22@gmail.com>
2020-07-12 14:51:27 -0400

    State y funciones para 'nuevo proyecto'

--- src/components/proyects/NewProyect.jsx
+++ src/components/proyects/NewProyect.jsx
@@ -1,18 +1,45 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const NewProyect = () => {
+
+  // State para nuevo proyecto
+  const [proyect, setProyect] = useState({
+    name: ''
+  });
+
+  // Extraer nombre de proyecto
+  const { name } = proyect;
+
+  // Lee contenido del input
+  const onChangeProyect = e => {
+    setProyect({
+      ...proyect,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  // Submit del Formulario
+  const onSubmitProyect = e => {
+    e.preventDefault()
+  }
+
   return (
     <Fragment>
       <button type="button" className="j-btn j-btn-block j-btn-primary">
         Nuevo Proyecto
       </button>
 
-      <form className="j-form-new-proyect">
+      <form 
+        className="j-form-new-proyect"
+        onSubmit={onSubmitProyect}
+      >
         <input
           type="text" 
           className="j-input-text"
           placeholder="Nombre Proyecto"
           name="name"
+          value={name}
+          onChange={onChangeProyect}
         />
 
         <input 
