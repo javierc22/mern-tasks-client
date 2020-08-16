5a13ed3125dcb28b9b71cd7678f48d40f8f86068
javierc22 <javier.ec.22@gmail.com>
2020-08-15 22:29:21 -0400

    Mostrando información del usuario autenticado
    Mostrar nombre en el header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import AuthContext from '../../context/authentication/authContext';
 
 const Header = () => {
+
+  // Extraer la información de autenticación
+  const authContext = useContext(AuthContext);
+  const { user, authenticateUser } = authContext;
+
+  useEffect( () => {
+    authenticateUser();
+  }, []);
+
   return (
     <header className="j-app-header">
-      <p className="j-name-user">Hola <span>Juan Pablo</span> </p>
-
+      { user ? <p className="j-name-user">Hola <span>{user.name}</span> </p> : null }
       <nav className="j-nav-main">
         <a href="#!">Cerrar Sesión</a>
       </nav>
